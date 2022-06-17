bd7e3f574cae2d25798b93fd9edbb4b938d531ac
PARK JAECHANG <73901174+unibit-123@users.noreply.github.com>
2022-06-18 02:29:47 +0900

    JS: Navbar menu toggle + project UI tweak

--- main.js
+++ main.js
@@ -12,7 +12,6 @@ document.addEventListener('scroll', () => {
 });
 
 // Handle scrolling when tapping on the navbar menu
-
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
     const target = event.target;
@@ -20,9 +19,16 @@ navbarMenu.addEventListener('click', (event) => {
     if (link == null) {
         return;
     }
-    
+    navbarMenu.classList.remove('open');
     scrollIntoView(link);
 });
+
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+    navbarMenu.classList.toggle('open');
+});
+
 
 const homeContactBtn = document.querySelector('.home__contact');
 homeContactBtn.addEventListener('click', () => {
@@ -34,6 +40,7 @@ const home = document.querySelector('.home__container');
 const homeHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
     home.style.opacity = 2.5 - window.scrollY / homeHeight;
+    navbarMenu.classList.remove('open');
 });
 
 // Show "arrow up" button when scrolling down
